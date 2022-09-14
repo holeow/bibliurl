76b3190781c05f2f87d8b6b1b822239f3c619e88
holeow <48766903+holeow@users.noreply.github.com>
2022-09-14 11:31:16 +0200

    CRUD Bookmark

--- app/Http/Controllers/API/BookmarkController.php
+++ app/Http/Controllers/API/BookmarkController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\BookmarkResource;
 use App\Models\Bookmark;
+use App\Models\Folder;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Validator;
 
 class BookmarkController extends BaseController
 {
@@ -15,7 +22,10 @@ class BookmarkController extends BaseController
      */
     public function index()
     {
-        
+        $bm = DB::table('Bookmark')->join('Folder','Bookmark.Folder', '=','Folder.ID')->where('Folder.WebUser','=', Auth::user()->id)->select("Bookmark.*")->get();
+
+        return $this->sendResponse(BookmarkResource::collection($bm),"Bookmarks trouvés avec succès");
+       
     }
 
     /**
@@ -26,7 +36,27 @@ class BookmarkController extends BaseController
      */
     public function store(Request $request)
     {
-        //
+        $input = $request->all();
+        $validator = Validator::make($input, [
+            "Name"=> "required|max:255",
+            "Comment" => "max:255",
+            "URL"=> "required|max:1023|url",
+            "ImgUrl"=> "max:1023|url",
+            "Folder"=> "integer"
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError("Validation error",$validator->errors(),400);
+        }
+        $folder = Folder::findorfail($input["Folder"]);
+        if(!Gate::allows(("access-folder"),$folder)){
+            return $this->sendError("Unauthorized access to folder","Unauthorized access to folder",403);
+        }
+        
+        $bookmark = new Bookmark($input);
+        $bookmark->CreationDate = now();
+        $bookmark->save();
+        return $this->sendResponse(new BookmarkResource($bookmark), "Bookmark créé avec succès");
     }
 
     /**
@@ -37,7 +67,10 @@ class BookmarkController extends BaseController
      */
     public function show(Bookmark $bookmark)
     {
-        //
+        if(!Gate::allows('access-bookmark',$bookmark)){
+            return $this->sendError(null,"Unauthorized access to bookmark",403);
+        }
+        return $this->sendResponse(new BookmarkResource($bookmark),"found with success");
     }
 
     /**
@@ -49,7 +82,35 @@ class BookmarkController extends BaseController
      */
     public function update(Request $request, Bookmark $bookmark)
     {
-        //
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            "Name"=> "required|max:255",
+            "Comment" => "max:255",
+            "URL"=> "required|max:1023|url",
+            "ImgUrl"=> "max:1023|url",
+            "Folder"=> "integer|required"
+        ]);
+        if(!Gate::allows("access-bookmark",$bookmark)){
+            return $this->sendError(null,"Unauthorized access to bookmark",403);
+        }
+        if($input["Folder"]!= $bookmark->Folder){
+            $folder = Folder::findorfail($input["Folder"]);
+
+            if(!Gate::allows("access-folder",$folder)){
+                return $this->sendError(null,"Unauthorized access to parent folder",403);
+            }
+        }
+
+        $bookmark->Name = $input["Name"] ?? null;
+        $bookmark->Comment = $input["Comment"] ?? null;
+        $bookmark->URL = $input["URL"] ?? null;
+        $bookmark->ImgUrl = $input["ImgUrl"] ?? null;
+        $bookmark->Folder = $input["Folder"] ?? null;
+        $bookmark->save();
+
+
+        return $this->sendResponse(null,"Updated with success");
     }
 
     /**
@@ -60,6 +121,10 @@ class BookmarkController extends BaseController
      */
     public function destroy(Bookmark $bookmark)
     {
-        //
+        if(!Gate::allows("access-bookmark",$bookmark)){
+            return $this->sendError(null,"Unauthorized access to bookmark",403);
+        }
+        $bookmark->delete();
+        return $this->sendResponse(null,"deleted with success");
     }
 }
