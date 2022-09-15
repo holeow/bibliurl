c0dc75ba4b43ca636ff01c07cf11c0c35a06c44a
holeow <48766903+holeow@users.noreply.github.com>
2022-09-15 15:31:36 +0200

    handle model not found

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Exceptions;
-
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -46,5 +46,24 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ModelNotFoundException && ($request->wantsJson() || str_starts_with($request->getRequestUri(),"/api/") )) {
+            return $this->send404(["errortype" => 404,"type"=> $exception->getModel(), "id"=> $exception->getIds()[0]]);
+        }
+
+        return parent::render($request, $exception);
+    }
+
+    public function send404($result)
+    {
+        $response = [
+            'success' => false,
+            'data'    => $result,
+            'message' => "Resource not found",
+        ];
+        return response()->json($response, 404);
     }
 }
