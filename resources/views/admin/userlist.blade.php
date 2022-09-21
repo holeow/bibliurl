15cff0f32777c979fe4729a36b734d980534261c
holeow <48766903+holeow@users.noreply.github.com>
2022-09-21 11:31:05 +0200

    indentation fix

--- resources/views/admin/userlist.blade.php
+++ resources/views/admin/userlist.blade.php
@@ -1,14 +1,25 @@
-<form action="{{route('admin.createuser')}}"> <input type="submit" value="Créer un utilisateur"></form>
+<form action="{{ route('admin.createuser') }}"> <input type="submit" value="Créer un utilisateur"></form>
 
 <table>
     @foreach ($users as $user)
-        <tr><td>{{$user->id}}</td><td>{{$user->name}}</td><td>{{$user->email}}</td>
+        <tr>
+            <td>{{ $user->id }}</td>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
             <td>
-                @if(!$user->isadmin)
-            <form method="POST" action="{{route("admin.makeadmin",['user'=>$user->id])}}">@csrf <input type="submit" value="rendre admin"></form>
-            @endif
-        </td>
-        <td> <form method="POST" action="{{route('admin.banuser',['user'=> $user->id])}}">@csrf <input type="submit" value="Bannir"></form></td>
-    </tr>
+                @if (!$user->isadmin)
+                    <form method="POST" action="{{ route('admin.makeadmin', ['user' => $user->id]) }}">
+                        @csrf
+                        <input type="submit" value="rendre admin">
+                    </form>
+                @endif
+            </td>
+            <td>
+                <form method="POST" action="{{ route('admin.banuser', ['user' => $user->id]) }}">
+                    @csrf 
+                    <input type="submit" value="Bannir">
+                </form>
+            </td>
+        </tr>
     @endforeach
 </table>
